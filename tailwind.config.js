21716d152f7c2a833c56c75c6e13bf0e978b41df
miacarmen <maitreyacarmen@gmail.com>
2022-05-15 15:41:20 -0700

    styled page, added social links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("daisyui")],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
 
-    daisyui: {
-      styled: true,
-      themes: true,
-      base: true,
-      utils: true,
-      logs: true,
-      rtl: false,
-      prefix: "",
-      darkTheme: "dark",
-    },
-  }
+  daisyui: {
+    styled: true,
+    themes: [
+      "valentine", "cupcake"
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+};
