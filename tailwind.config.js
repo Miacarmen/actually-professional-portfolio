4f325cfff4925fd48f77a9a58944816328cd80e5
miacarmen <maitreyacarmen@gmail.com>
2022-05-15 20:58:12 -0700

    added contact form page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    theme: {
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
